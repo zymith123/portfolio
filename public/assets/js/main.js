1533e14127a4a3070fa26e417d652fc27f34a74f
zymith123 <76258296+zymith123@users.noreply.github.com>
2025-07-10 22:47:39 +0800

    added new portfolio, link to codes, default dark theme

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -184,6 +184,9 @@ if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+} else {
+	document.body.classList['add'](darkTheme)
+	themeButton.classList['add'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
